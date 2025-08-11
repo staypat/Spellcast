f3915589f7d4af751b70dc8bb73379a52ccbedf1
Waeng <kennet10@uci.edu>
2025-08-11 15:10:28 -0700

    added mini turn system
    added part of the turn system that handles drawing and discard cards

--- Assets/Scripts/Cards/HandView.cs
+++ Assets/Scripts/Cards/HandView.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Splines;
 using DG.Tweening;
 using System.Collections;
+using System.Linq;
 
 public class HandView : MonoBehaviour
 {
@@ -14,6 +15,20 @@ public class HandView : MonoBehaviour
     {
         cards.Add(cardView);
         yield return UpdateCardPositions(0.15f);
+    }
+
+    public CardView RemoveCard(Card card)
+    {
+        CardView cardView = GetCardView(card);
+        if (cardView == null) return null;
+        cards.Remove(cardView);
+        StartCoroutine(UpdateCardPositions(0.15f));
+        return cardView;
+    }
+
+    private CardView GetCardView(Card card)
+    {
+        return cards.Where(cardView => cardView.card == card).FirstOrDefault();
     }
 
     private IEnumerator UpdateCardPositions(float duration)
