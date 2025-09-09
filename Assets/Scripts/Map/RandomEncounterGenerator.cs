cdab86244c1c6cf4677e2d76686f0d142fb0c4cb
Patrick <patrickhfhu@gmail.com>
2025-09-09 13:08:25 -0700

    Updated path encounters

--- Assets/Scripts/Map/RandomEncounterGenerator.cs
+++ Assets/Scripts/Map/RandomEncounterGenerator.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 public class RandomEncounterGenerator : MonoBehaviour
 {
-    public TMP_Text encounterText;
-    private EncounterType encounter;
+    [SerializeField] private Transform pathButtonParent;
+    [SerializeField] private GameObject pathButtonPrefab;
 
     private void Start()
     {
-        generateRandomEncounter(); // remove line if you move functionality to MapManager.cs
+        SpawnPaths();
     }
     public enum EncounterType
     {
@@ -32,29 +33,48 @@ public class RandomEncounterGenerator : MonoBehaviour
         }
         else
         {
-            return (EncounterType)Random.Range(0, 4);
+            return (EncounterType)Random.Range(1, 4);
         }
     }
 
-    public string getEncounterTypeName(EncounterType type)
+    public string encounterTypeToString(EncounterType type)
     {
         return type.ToString();
     }
 
-    public void generateRandomEncounter()
+    public void goToEncounter(EncounterType encounterType)
     {
-        encounter = getRandomEncounterType();
-        encounterText.text = getEncounterTypeName(encounter);
-    }
-
-    public void goToEncounter()
-    {
-        if (encounter == EncounterType.None)
+        if (encounterType == EncounterType.None)
         {
             return;
         }
 
         MapManager.instance.currentEncounterIndex++;
-        SceneManager.LoadScene(getEncounterTypeName(encounter));
+        SceneManager.LoadScene(encounterTypeToString(encounterType));
+    }
+
+    private void SpawnPaths()
+    {
+        int numPaths = (MapManager.instance.currentEncounterIndex == 6 || MapManager.instance.currentEncounterIndex == 7) ? 1 : Random.Range(2, 4);
+
+        float verticalSpacing = -60f;
+        float horizontalOffset = 55f;
+        float verticalOffset = 80f;
+
+        for (int i = 0; i < numPaths; i++)
+        {
+            // Make the path button and its position
+            GameObject pathButton = Instantiate(pathButtonPrefab, pathButtonParent);
+            float x = (i % 2 == 0) ? horizontalOffset : -horizontalOffset;
+            float y = i * verticalSpacing + verticalOffset;
+            pathButton.transform.localPosition = new Vector3(x, y, 0);
+
+            // Randomly assign encounter type
+            EncounterType encounter = getRandomEncounterType();
+
+            // Add onClick listener
+            Button button = pathButton.GetComponent<Button>();
+            button.onClick.AddListener(() => goToEncounter(encounter));
+        }
     }
 }
